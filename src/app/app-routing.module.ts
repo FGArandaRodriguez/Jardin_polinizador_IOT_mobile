fd98a27fc5465aa780744b65c57eac1125b34957
Franklin Giovanni Aranda Rodríguez <faranda@grainchain.io>
2023-02-22 00:42:42 -0500

    ADDED: connection to Firebase and create users and login with register users in this commit add the connection with firebase, add create new users add login eith register users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -8,7 +9,21 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then( m => m.AdminPageModule),
+    canActivate: [AuthGuard]
   }
 ];
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCEaNQI4AvpiMX_VllYqu4QBL3K1pfNAtU",
+    authDomain: "ionic-iot-4c0c4.firebaseapp.com",
+    projectId: "ionic-iot-4c0c4",
+    storageBucket: "ionic-iot-4c0c4.appspot.com",
+    messagingSenderId: "388297615659",
+    appId: "1:388297615659:web:109eb173848a1acdea7d6e",
+    measurementId: "G-JH3LQWE1TX"
+  }
 };
 
 /*
